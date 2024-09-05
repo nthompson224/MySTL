b69c4ccb1787244ab4d62d014cbfca412b6b7567
nthompson2 <nmt46@uakron.edu>
2024-09-05 13:25:39 -0400

    Add Array.hpp

---
+++ Array.hpp
@@ -0,0 +1,16 @@
+/*
+    Array.hpp
+
+    This file holds the interface file for Array.hpp
+*/
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template<typename T>
+class Array {
+    T* arr = nullptr;
+    int length = 0;
+};
+
+#endif

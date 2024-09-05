413d4ae5bf0e543f64b1ac95c83a48652ebd47e0
nthompson2 <nmt46@uakron.edu>
2024-09-05 13:38:17 -0400

    Add constructors and destructor to Array.hpp

--- Array.hpp
+++ Array.hpp
@@ -11,6 +11,42 @@ template<typename T>
 class Array {
     T* arr = nullptr;
     int length = 0;
+    
+public:
+    /**
+     * @brief Construct a new Array object
+     * 
+     * @param size size of the array
+     */
+    Array(int size) = default;
+
+    /**
+     * @brief Construct a new Array object with preallocated array
+     * 
+     * @param size size of the array
+     * @param data array data
+     */
+    Array(int size, T* data);
+
+    /**
+     * @brief Copy constructor for a new Array object
+     * 
+     * @param arr Array to copy from
+     */
+    Array(Array& arr);
+
+    /**
+     * @brief Move constructor for a new Array object
+     * 
+     * @param arr Array to move from
+     */
+    Array(Array&& arr);
+    
+    /**
+     * @brief Destroy the Array object
+     * 
+     */
+    ~Array();
 };
 
 #endif
